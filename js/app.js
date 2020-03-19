43fa78f7020d14ab9585bd86e873678b0aa8698d
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-03-19 09:49:16 -0500

    reuben made me

--- js/app.js
+++ js/app.js
@@ -1,88 +1,175 @@
 console.log("project 1");
 
-
 class Mole {
     constructor() {
-    	const colors = ['red', 'orange', 'brown']
-    	const randNum = Math.floor(Math.random() * colors.length)
-    	this.color = colors[randNum]
-    	this.clicked = false
+        const colors = ['red', 'pink']
+        const randNum = Math.floor(Math.random() * colors.length)
+        this.color = colors[randNum]
+        this.clicked = false
+    }
+    setClicked() {
+        this.clicked = true
     }
 }
 
+class Player {
+    constructor() {
+        this.score = score
+        this.turn = turn
+
+    }
+}
+
+
 const game = {
-		moles: [],
-		scoreBoard: document.querySelector('.score'),
-        intervalID: null,
-        timeElapsed: 0,
-        timesUp: false,
-        score: 0,
+    score1: 0,
+    score2: 0,
+    timeUp: false,
+    moles: [],
+    // holes: [],
 
-        // startTimer: function() {
-        //     this.printTimer()
-        //     this.intervalID = setInterval(() => {
-        //         this.timeElapsed++
-        //         this.printTimer()
-        //     	this.printValues()
-        //     }, 1000)
-        // },
 
-        // stopTimer: function() {
-        //     clearInterval(this.intervalID)
-        // },
+    createMoles: function() {
+        for (let i = 0; i < 8; i++) {
+            const mo = new Mole()
+            this.moles.push(mo)
+        }
+        this.printMoles()
+    },
 
-        // printTimer: function() {
-        //     const seconds = this.timeElapsed
-        //     let mm = Math.floor(seconds / 60)
-        //     let ss = seconds - (mm * 60)
-        //     if (ss < 10) {
-        //         ss = "0" + ss
-        //     }
-        //     console.log(`${mm}:${ss}`);
-        //     this.printValues()
-        // },
+    printMoles: function() {
+        const $molesContainer = $('.moles')
+        for (let i = 0; i < this.moles.length; i++) {
+            const $div = $(`<div data-which-mole="${i}"></div>`).addClass('mole')
+            $div.css('background-color', this.moles[i].color)
+            $molesContainer.append($div)
+        }
+    },
 
-        // printValues: function() {
-        // 	const timer = $('#timer')
-        // 	timer.text($`{timeElapsed}`)
-        // },
-
-        // wack: function(event) {
-        // 	if(!timesUp) {
-        // 		scoreBoard.classList.add('add')
-        // 		score++
-        // 		scoreBoard.textContent = score
-        // 	}
-        // },
-
-        // checkValidWack: function($mole) {
-        // 	const indexOfMoleClicked = $mole.data('whichMole')
-        // 	const mole = this.moles[indexOfMoleClicked]
-        // 	if(this.mole === )
-        // },
-
-        createMoles: function() {
-        	for(let i = 0; i < 12; i++) {
-        		const ml = new Mole()
-        		this.moles.push(ml)
-        	}
-        	this.printMoles()
-        },
-
-        printMoles: function() {
-        	const $molesContainer = $('.moles')
-        	for(let i = 0; i < this.moles.length; i++) {
-        		const $div = $(`div data-which-mole="${i}"></div>`).addClass('mole')
-        		$div.css('background-color', this.moles[i].color)
-        		$molesContainer.append($div)
-        	}
-        },
-
-        start: function() {
-        	scoreBoard.textContent = this.score
-        	scoreBoard.classList.remove('add')
+    checkValidWack: function($mole) {
+        const indexOfMoleClicked = $mole.data('whichMole')
+        const mole = this.moles[indexOfMoleClicked]
+        if (mole.clicked === false) {
+            mole.setClicked()
+            $mole.css('opacity', 0)
+            //display messagee hit!
+        } else {
+            console.log("bad");
+            // display message miss!
         }
     }
+}
+
+
+$('.moles').on('click', (event) => {
+    const $moleClicked = $(event.target)
+    game.checkValidWack($moleClicked)
+    // makes them disappear -- so works -- need to make them smash (another image?)
+    // also need to add to the score
+
+})
+
+$('.button').on('click', (event) => {
+    const $btnStart = $(event.target)
+    game.createMoles($btnStart)
+    //need to make the button stop printing moles after clicked once
+})
+
+
+
+
+// moles.forEach(mole => {
+//   mole.addEventListener('click', wack);
+// });
+
+// const btnStart = document.querySelector('button');
+// btnStart.addEventListener('click', createMoles);
+
+
+
+
+// class Mole {
+//     constructor() {
+//     	const colors = ['red', 'orange', 'brown']
+//     	const randNum = Math.floor(Math.random() * colors.length)
+//     	this.color = colors[randNum]
+//     	this.clicked = false
+//     }
+// }
+
+// const game = {
+// 		moles: [],
+// 		scoreBoard: document.querySelector('.score'),
+//         intervalID: null,
+//         timeElapsed: 0,
+//         timesUp: false,
+//         score: 0,
+
+// startTimer: function() {
+//     this.printTimer()
+//     this.intervalID = setInterval(() => {
+//         this.timeElapsed++
+//         this.printTimer()
+//     	this.printValues()
+//     }, 1000)
+// },
+
+// stopTimer: function() {
+//     clearInterval(this.intervalID)
+// },
+
+// printTimer: function() {
+//     const seconds = this.timeElapsed
+//     let mm = Math.floor(seconds / 60)
+//     let ss = seconds - (mm * 60)
+//     if (ss < 10) {
+//         ss = "0" + ss
+//     }
+//     console.log(`${mm}:${ss}`);
+//     this.printValues()
+// },
+
+// printValues: function() {
+// 	const timer = $('#timer')
+// 	timer.text($`{timeElapsed}`)
+// },
+
+// wack: function(event) {
+// 	if(!timesUp) {
+// 		scoreBoard.classList.add('add')
+// 		score++
+// 		scoreBoard.textContent = score
+// 	}
+// },
+
+// checkValidWack: function($mole) {
+// 	const indexOfMoleClicked = $mole.data('whichMole')
+// 	const mole = this.moles[indexOfMoleClicked]
+// 	if(this.mole === )
+// },
+
+//     createMoles: function() {
+//     	for(let i = 0; i < 12; i++) {
+//     		const ml = new Mole()
+//     		this.moles.push(ml)
+//     	}
+//     	this.printMoles()
+//     },
+
+//     printMoles: function() {
+//     	const $molesContainer = $('.moles')
+//     	for(let i = 0; i < this.moles.length; i++) {
+//     		const $div = $(`div data-which-mole="${i}"></div>`).addClass('mole')
+//     		$div.css('background-color', this.moles[i].color)
+//     		$molesContainer.append($div)
+//     	}
+//     },
+
+//     start: function() {
+//     	scoreBoard.textContent = this.score
+//     	scoreBoard.classList.remove('add')
+//     }
+// }
 
 
 
@@ -98,15 +185,15 @@ const game = {
 // })
 
 
-        // start: function() {
-        //     score = 0
-        //     scoreBoard.textContent = score
-        //     timesOut = false
-        //     scoreBoard.classList.remove('add')
-        //     startScreen.classList.add('hide')
-        //     startTimer()
-        //     // showMole()
-        // }
+// start: function() {
+//     score = 0
+//     scoreBoard.textContent = score
+//     timesOut = false
+//     scoreBoard.classList.remove('add')
+//     startScreen.classList.add('hide')
+//     startTimer()
+//     // showMole()
+// }
 
 // 	showMole: function() {
 // 	// mole image appears in random box in the container
@@ -136,7 +223,7 @@ const game = {
 // 	},
 
 
-  // listeners   ////
+// listeners   ////
 
 
 // moles.forEach(mole => {
@@ -148,15 +235,4 @@ const game = {
 
 
 
-
 // mole.addEventListener('click', ___) // or on.('click', event)
-
-
-
-
-
-
-
-
-
-
